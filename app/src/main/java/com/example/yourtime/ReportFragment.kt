079a9dd18e76764380dd8faa2cdf5fcc81360ba1
hao18 <hao18@vt.edu>
2022-11-17 22:42:32 -0500

    report fragment

--- app/src/main/java/com/example/yourtime/ReportFragment.kt
+++ app/src/main/java/com/example/yourtime/ReportFragment.kt
@@ -1,10 +1,20 @@
 package com.example.yourtime
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 
 
 /**
@@ -15,13 +25,12 @@ import android.view.ViewGroup
 class ReportFragment : Fragment() {
 
     //pie chart view
-
+    private lateinit var reportPieChart: PieChart
+    private lateinit var dateSelector: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
 
-        }
     }
 
     override fun onCreateView(
@@ -29,6 +38,53 @@ class ReportFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false)
+        val view = inflater.inflate(R.layout.fragment_report, container, false)
+        reportPieChart = view.findViewById(R.id.reportPieChart)
+        dateSelector = view.findViewById(R.id.dataSelector)
+        setupPieChart()
+        loadPieChart()
+        return view
+    }
+
+    private fun setupPieChart() {
+        reportPieChart.setDrawHoleEnabled(true)
+        reportPieChart.setUsePercentValues(true)
+        reportPieChart.setEntryLabelTextSize(12F)
+        reportPieChart.setEntryLabelColor(Color.BLACK)
+        reportPieChart.setCenterText("Your Time")
+        reportPieChart.setCenterTextSize(24F)
+        reportPieChart.getDescription().setEnabled(false)
+        val l: Legend = reportPieChart.getLegend()
+        l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        l.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        l.orientation = Legend.LegendOrientation.VERTICAL
+        l.setDrawInside(false)
+        l.isEnabled = true
+    }
+
+    fun loadPieChart() {
+        val entries = ArrayList<PieEntry>()
+        entries.add(PieEntry(0.25f, "activity1"))
+        entries.add(PieEntry(0.33f, "activity2"))
+        entries.add(PieEntry(0.25f, "activity3"))
+        entries.add(PieEntry(0.17f, "activity4"))
+
+        val colors = ArrayList<Int>()
+        for (i in ColorTemplate.MATERIAL_COLORS) {
+            colors.add(i)
+        }
+        val dataSet = PieDataSet(entries, "time")
+        dataSet.setColors(colors)
+
+        val data = PieData(dataSet)
+        data.setDrawValues(true)
+        data.setValueFormatter(PercentFormatter(reportPieChart))
+        data.setValueTextSize(12f)
+        data.setValueTextColor(Color.BLACK)
+
+        reportPieChart.setData(data);
+        reportPieChart.invalidate();
+
+        reportPieChart.animateY(1400, Easing.EaseInOutQuad);
     }
 }
--- app/src/main/java/com/example/yourtime/TimerFragment.kt
+++ app/src/main/java/com/example/yourtime/TimerFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,16 +19,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TimerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var reportButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,7 +32,14 @@ class TimerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false)
+        val view = inflater.inflate(R.layout.fragment_timer, container, false)
+        reportButton = view.findViewById(R.id.ReportButton)
+
+        reportButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_timerFragment_to_reportFragment)
+        }
+
+        return view
     }
 
     companion object {

328b378e279678997d035b4a81e7c69294b13e0e
hao18 <hao18@vt.edu>
2022-12-02 03:22:50 -0500

    continue implementing report fragment

--- app/src/main/java/com/example/yourtime/ReportFragment.kt
+++ app/src/main/java/com/example/yourtime/ReportFragment.kt
@@ -1,11 +1,14 @@
 package com.example.yourtime
 
+import android.app.DatePickerDialog
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
@@ -15,6 +18,9 @@ import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 /**
@@ -26,11 +32,13 @@ class ReportFragment : Fragment() {
 
     //pie chart view
     private lateinit var reportPieChart: PieChart
-    private lateinit var dateSelector: EditText
+    private lateinit var text: TextView
+    private lateinit var button: Button
+
+    var cal = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -40,10 +48,54 @@ class ReportFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_report, container, false)
         reportPieChart = view.findViewById(R.id.reportPieChart)
-        dateSelector = view.findViewById(R.id.dataSelector)
+        button = view.findViewById(R.id.button)
+        text = view.findViewById(R.id.input)
+
+        var cal = Calendar.getInstance()
+
+        val model = TimeViewModel()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            text.text = sdf.format(cal.time)
+
+        }
+
+        button.setOnClickListener {
+            context?.let { it1 ->
+                DatePickerDialog(
+                    it1, dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+
+            val entriesForOneDay = ArrayList<Event>()
+            
+            loadPieChart(calculateTime(model.allEvents.value!!))
+        }
+
         setupPieChart()
-        loadPieChart()
+
         return view
+    }
+
+    private fun calculateTime(events: List<Event>) : FloatArray {
+        var work: Float = 0.1f
+        var exercise: Float = 0.2f
+        var restanrant: Float = 0.3f
+        var other: Float = 0.4f
+        var sum: Float = 0f
+
+
+
+
+        return floatArrayOf(work, exercise, restanrant, other)
     }
 
     private fun setupPieChart() {
@@ -62,12 +114,12 @@ class ReportFragment : Fragment() {
         l.isEnabled = true
     }
 
-    fun loadPieChart() {
+    fun loadPieChart(arr: FloatArray) {
         val entries = ArrayList<PieEntry>()
-        entries.add(PieEntry(0.25f, "activity1"))
-        entries.add(PieEntry(0.33f, "activity2"))
-        entries.add(PieEntry(0.25f, "activity3"))
-        entries.add(PieEntry(0.17f, "activity4"))
+        entries.add(PieEntry(arr[0], "work"))
+        entries.add(PieEntry(arr[1], "exercise"))
+        entries.add(PieEntry(arr[2], "restaurant"))
+        entries.add(PieEntry(arr[3], "other"))
 
         val colors = ArrayList<Int>()
         for (i in ColorTemplate.MATERIAL_COLORS) {
